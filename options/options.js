50e4d0f794276ee44c75bb22099e84e4ccf2ab24
Crowdbotics <team@crowdbotics.com>
2023-08-22 17:13:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-disk-43024",
+  url: "https://dry-disk-43024.botics.co",
+  api: "https://dry-disk-43024.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
